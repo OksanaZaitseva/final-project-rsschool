11829c8c9f2c07a94fbac07719abd0bd5ad236ca
OksanaZaitseva <o.zaitseva@softteco.com>
2022-04-30 17:14:36 +0300

    Add classifier switcher as a separate module

--- src/forest_ml/train.py
+++ src/forest_ml/train.py
@@ -3,14 +3,11 @@ from pathlib import Path
 import click
 import pandas as pd
 
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.svm import SVC
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
-
 from sklearn.metrics import accuracy_score, f1_score, recall_score, precision_score
 from sklearn.model_selection import train_test_split
 
+from .data import get_dataset
+from .pipeline import create_pipeline
 
 @click.command()
 @click.option(
@@ -19,25 +16,47 @@ from sklearn.model_selection import train_test_split
     default="data/train.csv",
     type=click.Path(exists=True, dir_okay=False, path_type=Path),
 )
-@click.option("--random-state", default=42, type=int)
+@click.option(
+    "-s",
+    "--save-model-path",
+    default="data/model.joblib",
+    type=click.Path(dir_okay=False, writable=True, path_type=Path),
+    show_default=True,
+)
+@click.option(
+    "--random-state",
+    default=42,
+    type=int)
+
 @click.option(
     "--test-split-ratio",
     default=0.2,
     type=click.FloatRange(0, 1, min_open=True, max_open=True),
 )
-def train(dataset_path: Path, random_state: int, test_split_ratio: float) -> None:
-    dataset = pd.read_csv(dataset_path, index_col='Id')
-    click.echo(f"Dataset shape: {dataset.shape}.")
-    features = dataset.drop('Cover_Type', axis=1)
-    target = dataset['Cover_Type']
 
-    features_train, features_val, target_train, target_val = train_test_split(
-        features, target, test_size=test_split_ratio, random_state=random_state
+@click.option(
+    "--use-scaler",
+    default=True,
+    type=bool,
+    show_default=True,
+)
+
+def train(dataset_path: Path,
+          save_model_path: Path,
+          random_state: int,
+          test_split_ratio: float,
+          use_scaler: bool,
+
+          ) -> None:
+    features_train, features_val, target_train, target_val = get_dataset(
+        dataset_path,
+        random_state,
+        test_split_ratio,
     )
-    classifier = RandomForestClassifier(random_state=random_state).fit(
-        features_train, target_train
-    )
-    predict = classifier.predict(features_val)
+    pipeline = create_pipeline(use_scaler, random_state)
+    pipeline.fit(features_train, target_train)
+
+    predict = pipeline.predict(features_val)
 
     metric_results = []
     for metric in [accuracy_score, f1_score, recall_score, precision_score]:
