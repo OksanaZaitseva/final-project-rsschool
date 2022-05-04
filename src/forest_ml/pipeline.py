b36d58a019a27ff9741f8eb3381bcd517bfc0961
OksanaZaitseva <o.zaitseva@softteco.com>
2022-05-04 11:43:54 +0300

    Add preprocess to pipline

--- src/forest_ml/pipeline.py
+++ src/forest_ml/pipeline.py
@@ -1,29 +1,47 @@
-from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier, GradientBoostingClassifier
 
 from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import StandardScaler
-# from .classifier_switcher import ClfSwitcher
+from sklearn.preprocessing import StandardScaler, QuantileTransformer, PolynomialFeatures
+from sklearn.compose import ColumnTransformer
 
-classifiers={'DecisionTreeClassifier()': DecisionTreeClassifier(),
-            'RandomForestClassifier()': RandomForestClassifier(),
-            'AdaBoostClassifier()': AdaBoostClassifier(),
-            'KNeighborsClassifier()': KNeighborsClassifier(),
-            'SVC()': SVC()}
+from .classifier_switcher import ClfSwitcher
 
+classifiers = {'DecisionTreeClassifier()': DecisionTreeClassifier(),
+               'RandomForestClassifier()': RandomForestClassifier(),
+               'AdaBoostClassifier()': AdaBoostClassifier(),
+               'GradientBoostingClassifier()': GradientBoostingClassifier(),
+               'SVC()': SVC()}
 
-# try:
-#     function=dispatcher[w]
-# except KeyError:
-#     raise ValueError('invalid input')
+norm_col = ['Elevation', 'Aspect', 'Slope', 'Horizontal_Distance_To_Hydrology',
+            'Vertical_Distance_To_Hydrology', 'Horizontal_Distance_To_Roadways',
+            'Hillshade_9am', 'Hillshade_Noon', 'Hillshade_3pm',
+            'Horizontal_Distance_To_Fire_Points']
+
 
 def create_pipeline(
-    use_scaler: bool, estimator: str
+        use_scaler: bool, uniform: bool, poly: bool,
+        estimator: str = 'DecisionTreeClassifier()', degree: int = 2, norm_col=norm_col
 ) -> Pipeline:
+    preproc_norm = ColumnTransformer(
+        [('quantile', QuantileTransformer(output_distribution='normal', random_state=random_state), norm_col)],
+        remainder='passthrough')
+
+    preproc_poly = ColumnTransformer(
+        [('polynom', PolynomialFeatures(degree, interaction_only=True), norm_col)],
+        remainder='passthrough')
+
     pipeline_steps = []
     if use_scaler:
         pipeline_steps.append(("scaler", StandardScaler()))
-    pipeline_steps.append(("classifier", classifiers[estimator]))
+
+    if uniform:
+        pipeline_steps.append(("uniform", preproc_norm))
+
+    if poly:
+        pipeline_steps.append(("polynom", preproc_poly))
+
+    pipeline_steps.append(("classifier", ClfSwitcher(classifiers[estimator])))
     return Pipeline(steps=pipeline_steps)
+
